1b196ec12055b3f036b9f64287cd8b496a58f1a4
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2025-03-08 17:18:57 +0700

    initiate project

---
+++ cyberkit-server/src/main/java/com/cyberkit/cyberkit_server/CyberkitServerApplication.java
@@ -0,0 +1,13 @@
+package com.cyberkit.cyberkit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberkitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberkitServerApplication.class, args);
+	}
+
+}
